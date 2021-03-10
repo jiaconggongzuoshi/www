fe6af91a8662ac9630e399dd63b3bf2cb02525ac
嘉聪 <48845951+jiaconggongzuoshi@users.noreply.github.com>
2021-03-10 07:54:37 -0600

    Update verification.js

--- js/verification.js
+++ js/verification.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;  
     var pwd=document.getElementById("pwd").value;
     if(userName=="susu" &&  pwd=="susu"){
-    	$('#h').text("欢迎进入（来自你的宝贝聪）！");
+    	$('#h').text("点我进入");
 		event.preventDefault();
 		$('form').fadeOut(500);
 		$('.wrapper').addClass('form-success');
